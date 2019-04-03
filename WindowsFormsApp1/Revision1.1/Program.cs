352e145fc936b218a7a2cc964232e94b2ceb373c
maxiclass <40461151+maxiclass@users.noreply.github.com>
2019-04-03 11:58:28 +0300

    3.4.2019
    11.57

--- WindowsFormsApp1/Revision1.1/Program.cs
+++ WindowsFormsApp1/Revision1.1/Program.cs
@@ -24,3 +24,53 @@ namespace UtilityApp
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+namespace UtilityApp
+{
+     class InitFunctions
+    {
+        public static void InitFunctionStep1()
+        {
+        SCD.IntRecordNumber = Convert.ToInt32(ExcelDefine.Sheet2.Cells[9, 4].Value());
+        SCD.IntEntryNumber = Convert.ToInt32(ExcelDefine.Sheet2.Cells[10, 4].Value());
+
+        SCD.IntOnlineTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[12, 4].Value());
+        SCD.IntOfflineTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[13, 4].Value());
+
+        SCD.IntTotalOnlineTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[15, 4].Value());
+        SCD.IntTotalOfflineTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[16, 4].Value());
+
+        SCD.StrTodayDate = ExcelDefine.Sheet2.Cells[18, 4].Value.ToString();
+        SCD.StrDayOfTheWeek = ExcelDefine.Sheet2.Cells[19, 4].Value.ToString();
+
+        SCD.IntPlannedWorkingTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[21, 4].Value());
+        SCD.IntPlannedBreakTime = Convert.ToInt32(ExcelDefine.Sheet2.Cells[22, 4].Value());
+
+        SCD.StrCurrentTaskLink = ExcelDefine.Sheet2.Cells[24, 4].Value.ToString();
+        SCD.StrCurrentTaskStatus = ExcelDefine.Sheet2.Cells[25, 4].Value.ToString();
+        SCD.IntCurrentTaskPercent = Convert.ToInt32(ExcelDefine.Sheet2.Cells[26, 4].Value());
+
+        }
+
+        public static void InitFunctionStep2()
+        {
+
+        }
+
+        public static void InitFunctionStep3()
+        {
+
+        }
+
+    }
+     
+}
